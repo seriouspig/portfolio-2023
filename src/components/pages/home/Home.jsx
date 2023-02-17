715123bbdb1ee5ccf77206c76a6d93d893873908
Piotr Gryko <pigryko@yahoo.com>
2023-02-17 17:33:09 +0000

    Added Draw SVG

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,20 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import classes from "./Home.module.css";
-import Type from './Type';
+import Type from "./Type";
+import { ReactComponent as PiotrSvg } from "../../../assets/logos/test.svg";
+import DrawSVG from "react-svg-drawing";
 
 const Home = () => {
+
+
   return (
     <>
       <div className={classes["home-container"]}>
         <div className={classes["info-container"]}>
-          <div class="section-title">
+          <div className="section-title">
             <h2>Hello I'm Piotr Gryko</h2>
-            <div class="underline"></div>
+            <div className="underline"></div>
             <Type />
-            <p class="projects-text">
+            <p className="projects-text">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
               provident, quod voluptatum porro ea accusantium ex ad molestiae.
             </p>
+            <DrawSVG duration="10000" width="100%" target=".st1">
+                <PiotrSvg />
+            </DrawSVG>
+
           </div>
         </div>
 
