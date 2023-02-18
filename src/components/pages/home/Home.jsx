23aba6d8748359078e5598e3114d61df6b4434b6
Piotr Gryko <pigryko@yahoo.com>
2023-02-18 16:21:42 +0000

    Added landing 3d image

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import "./Home.css";
 import Type from "./Type";
 import { ReactComponent as PiotrSvg } from "../../../assets/logos/test.svg";
 import { ReactComponent as PiotrSketchSvg } from "../../../assets/logos/sketch.svg";
+import landingImage1 from "../../../assets/images/landing_page_3.png"
 
 
 const Home = () => {
@@ -12,7 +13,11 @@ const Home = () => {
     <>
       <div className="home-container">
         <div className="info-container">
-          <PiotrSketchSvg style={{ width: "100%", display: "block" }} />
+          <div className="landing-image-container">
+            <img src={landingImage1} alt="" />
+          </div>
+
+          {/* <PiotrSketchSvg style={{ width: "100%", display: "block" }} /> */}
           <div className="section-title">
             <h2>Hello I'm Piotr Gryko</h2>
             <div className="underline"></div>
